d6d78d694e16a5218b22852bea85692dc2b1e1dc
Jordan Holmes <jordanstholmes@gmail.com>
2023-10-23 14:34:18 -0700

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
+  return Array(num).fill(str).join('');
 };
 
 // Do not edit below this line
