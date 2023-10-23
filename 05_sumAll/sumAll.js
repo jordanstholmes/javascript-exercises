278b0993968e2b9b5c76f4ff570860a5795ddabe
Jordan Holmes <jordanstholmes@gmail.com>
2023-10-23 15:26:13 -0700

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  const hasNegativeArg = num1 < 0 || num2 < 0;
+  const hasNonNumberArg = typeof num1 !== 'number' || typeof num2 !== 'number';
+  if (hasNegativeArg || hasNonNumberArg) return 'ERROR';
 
+  const max = Math.max(num1, num2);
+  const min = Math.min(num1, num2);
+
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
