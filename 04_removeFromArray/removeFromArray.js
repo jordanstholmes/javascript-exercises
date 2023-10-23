abed261a8ed74e4eb77b220eef409ebda3714c24
Jordan Holmes <jordanstholmes@gmail.com>
2023-10-23 14:56:03 -0700

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...itemsToRemove) {
+  const items = new Set(itemsToRemove);
+  return arr.filter((element) => {
+    return !items.has(element);
+  });
 };
 
 // Do not edit below this line
