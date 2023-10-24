81b411e8e1cdd433df3eed4cf088a981953f0826
Jordan Holmes <jordanstholmes@gmail.com>
2023-10-23 20:45:43 -0700

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function roundTo(num, places) {
+  return Math.round((num + Number.EPSILON) * 10**places) / 10**places;
+}
+
+const convertToCelsius = function(temp) {
+  return roundTo((temp - 32) * (5/9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return roundTo(temp * (9/5) + 32, 1);
 };
 
 // Do not edit below this line
