4ea0a2abe5bdbda8642056541e13bcc3288eb73a
Jordan Holmes <jordanstholmes@gmail.com>
2023-10-25 10:24:22 -0700

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const justLetters = removeNonAlphaNumeric(str).toLowerCase();
+  let i = 0;
+  let j = justLetters.length - 1;
+  while (j > i) {
+    if (justLetters[i] !== justLetters[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+  return true;
 };
+
+function removeNonAlphaNumeric(str) {
+  let newStr = '';
+  for (const char of str) {
+    if (isAlphaNumeric(char)) {
+      newStr += char;
+    }
+  }
+  return newStr;
+}
+
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+  const isUpperCase = code >= 65 && code <= 90;
+  const isLowerCase = code >= 97 && code <= 122;
+  const isNumber = code >= 48 && code <= 57;
+  return isUpperCase || isLowerCase || isNumber;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
