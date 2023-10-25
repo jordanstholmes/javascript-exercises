342759240496a755846d47ef65943cb758c0aa75
Jordan Holmes <jordanstholmes@gmail.com>
2023-10-25 10:34:15 -0700

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  if (member < 0) return 'OOPS';
+  if (member === 0) return 0;
+  if (member <= 2) return 1;
 
+  const prev = [1, 1]
+  let curr;
+  for (let i = 3; i <= member; i++) {
+    curr = prev[0] + prev[1];
+    prev[0] = prev[1];
+    prev[1] = curr;
+  }
+  return curr;
 };
 
 // Do not edit below this line
